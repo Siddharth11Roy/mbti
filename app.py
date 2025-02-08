511e02eacc9c4eebf5ebf8ff1afd6c3fe6512d82
Siddharth11Roy <155230297+Siddharth11Roy@users.noreply.github.com>
2025-02-09 00:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -352,7 +352,7 @@ st.markdown(
     """
     <style>
         body {
-            background: linear-gradient(to right, #2C2E43, #8E2DE2);
+            background: linear-gradient(to right, #2C2E43, #E03B8B);
             color: white;
         }
         .stApp {
